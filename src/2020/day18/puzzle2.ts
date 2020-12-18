a8a9660a558e7edd4c0005c6a5fa5ada37605e7d
Benjamin Schirmer <benjamin.schirmer@de.bosch.com>
2020-12-18 11:10:26 +0100

    day18. most ugly code but works ...

---
+++ src/2020/day18/puzzle2.ts
@@ -0,0 +1,85 @@
+import { assert } from 'console';
+import { assertEquals, splitFileContents } from '../helper';
+
+(() => {
+    enum Operator {
+        Add,
+        Mul,
+    };
+
+    interface Example {
+        equation: string;
+        result: number;
+    }
+
+    const examples: Example[] = [
+        { equation: '1 + (2 * 3) + (4 * (5 + 6))', result: 51 },
+        { equation: '2 * 3 + (4 * 5)', result: 46 },
+        { equation: '5 + (8 * 3 + 9 + 3 * 4 * 3)', result: 1445 },
+        { equation: '5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))', result: 669060 },
+        { equation: '((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2', result: 23340 },
+    ]
+
+    const equations = splitFileContents('day18/input.txt', '\n');
+
+    const runEquation = (line: string) => {
+        const runSubEquation = (equation: string) => {
+            let subResult = 0;
+            let operator = Operator.Add;
+            let elements = equation.split(' ');
+
+            while (true) {
+                const addPos = elements.findIndex(el => el === '+');
+                if (addPos === -1) {
+                    break;
+                }
+                elements[addPos - 1] = `${parseInt(elements[addPos - 1], 10) + parseInt(elements[addPos + 1], 10)}`;
+                elements[addPos] = null;
+                elements[addPos + 1] = null;
+                elements = elements.filter(el => el !== null);
+            }
+            elements
+                .filter(el => el !== null)
+                .forEach(el => {
+                switch (el) {
+                    case '+':
+                        operator = Operator.Add;
+                        break;
+                    case '*':
+                        operator = Operator.Mul;
+                        break;
+                    default:
+                        const num = parseInt(el, 10);
+                        switch (operator) {
+                            case Operator.Add:
+                                subResult += num;
+                                break;
+                            case Operator.Mul:
+                                subResult *= num;
+                                break;
+                        }
+                        break;
+                }
+            });
+            return subResult;
+        };
+
+        while (true) {
+            const subEquation = line.match(/\(([0-9+*\s]+)\)/);
+            if (subEquation) {
+                const equationResult = runSubEquation(subEquation[1]);
+                line = line.replace(subEquation[0], `${equationResult}`);
+            }
+            else {
+                return runSubEquation(line);
+            }
+        }
+    };
+
+    examples.forEach(example => {
+        assertEquals(example.result, runEquation(example.equation));
+    });
+
+    const result = equations.reduceRight((prev, line) => prev + runEquation(line), 0);
+    console.log('Sum of all equations is: ', result);
+})();
