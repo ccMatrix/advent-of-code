8ad1475907da64da2773c4ab976cc34ba67ec04d
Benjamin Schirmer (HC/BDO-S3) <Benjamin.Schirmer@de.bosch.com>
2023-12-08 10:55:31 +0100

    make traverseMap function reusable for part1 as well

--- src/2023/day8/day8.ts
+++ src/2023/day8/day8.ts
@@ -14,7 +14,7 @@ interface INode {
 
 const parseMap = (data: string[]) => {
     const map = new Map<string, INode>();
-    input.forEach(line => {
+    data.forEach(line => {
         const match = line.match(/([A-Z0-9]{3})\s=\s\(([A-Z0-9]{3}),\s([A-Z0-9]{3})\)/);
         if (match) {
             const [_, value, left, right] = match;
@@ -25,88 +25,62 @@ const parseMap = (data: string[]) => {
     return map;
 }
 
-(() => {
-    const instructions = input[0].trim().split('');
-    const traverseMap = (map: Map<string, INode>, instructions: string[]) => {
-        let currentNode = 'AAA';
-        const endPoint = 'ZZZ';
-        let currentPos = 0;
-        let totalSteps = 0;
-        while (true) {
-            const direction = instructions[currentPos];
+const traverseMap = (map: Map<string, INode>, instructions: string[], startNode: string, endNode: string) => {
+    let currentNodes = Array.from(map.keys()).filter(n => n.endsWith(startNode));
+    console.log('Traversing map for', currentNodes.length, 'starting Nodes');
+    const nodeCounter = new Map<string, number>();
+    let currentPos = 0;
+    let totalSteps = 0;
+    while (true) {
+        const direction = instructions[currentPos];
+        currentNodes = currentNodes.map(currentNode => {
             const node = map.get(currentNode);
             switch (direction) {
                 case 'L':
-                    currentNode = node.left;
-                    break;
+                    return node.left;
                 case 'R':
-                    currentNode = node.right;
-                    break;
+                    return node.right;
             }
-            totalSteps++;
-            if (currentNode === endPoint) {
-                return totalSteps;
-            }
+        });
+        totalSteps++;
+        currentNodes
+            .filter(n => n.endsWith(endNode))
+            .forEach((n) => {
+                console.log('Node finished after', totalSteps, 'steps');
+                nodeCounter.set(n, totalSteps);
+            });
+        currentNodes = currentNodes.filter(n => !n.endsWith(endNode));
+        if (currentNodes.length === 0) {
+            console.log('All nodes are done');
+            return Array.from(nodeCounter.values());
+        }
 
-            if (++currentPos >= instructions.length) {
-                currentPos = 0;
-            }
+        if (++currentPos >= instructions.length) {
+            currentPos = 0;
         }
     }
+}
+
+(() => {
+    const instructions = input[0].trim().split('');
 
     const parsedMap = parseMap(input);
-    const steps = traverseMap(parsedMap, instructions);
-    console.log('Part1:', steps, ' steps');
+    const steps = traverseMap(parsedMap, instructions, 'AAA', 'ZZZ');
+    console.log('Part1:', steps.pop(), 'steps');
 })();
 
 (() => {
     const instructions = input[0].trim().split('');
 
-    const traverseMap = (map: Map<string, INode>, instructions: string[]) => {
-        /**
-         * greatest-common-divisor and least-common-multiple methods from ruffle1986:
-         * https://github.com/ruffle1986?tab=repositories&q=common
-         */
-        const gcd = (a: number, b: number): number => (!b) ? a : gcd(b, a % b);
-        const lcm = (...args: number[]) => args.reduce((a, b) => Math.abs(a * b) / gcd(a, b));
+    /**
+     * greatest-common-divisor and least-common-multiple methods from ruffle1986:
+     * https://github.com/ruffle1986?tab=repositories&q=common
+     */
+    const gcd = (a: number, b: number): number => (!b) ? a : gcd(b, a % b);
+    const lcm = (...args: number[]) => args.reduce((a, b) => Math.abs(a * b) / gcd(a, b));
 
-        let currentNodes = Array.from(map.keys()).filter(n => n.endsWith('A'));
-        console.log('Part2:', 'Traversing map for', currentNodes.length, 'starting Nodes');
-        const nodeCounter = new Map<string, number>();
-        let currentPos = 0;
-        let totalSteps = 0;
-        while (true) {
-            const direction = instructions[currentPos];
-            currentNodes.forEach((currentNode, index) => {
-                const node = map.get(currentNode);
-                switch (direction) {
-                    case 'L':
-                        currentNodes[index] = node.left;
-                        break;
-                    case 'R':
-                        currentNodes[index] = node.right;
-                        break;
-                }
-            })
-            totalSteps++;
-            currentNodes
-                .filter(n => n.endsWith('Z'))
-                .forEach((n) => {
-                    console.log('Part2:', 'Node finished after', totalSteps, 'steps');
-                    nodeCounter.set(n, totalSteps);
-                });
-            currentNodes = currentNodes.filter(n => !n.endsWith('Z'));
-            if (currentNodes.length === 0) {
-                console.log('Part2:', 'All nodes are done');
-                return lcm(...Array.from(nodeCounter.values()));
-            }
-
-            if (++currentPos >= instructions.length) {
-                currentPos = 0;
-            }
-        }
-    }
     const parsedMap = parseMap(input);
-    const steps = traverseMap(parsedMap, instructions);
-    console.log('Part2:', steps, ' steps');
+    const steps = traverseMap(parsedMap, instructions, 'A', 'Z');
+    const totalSteps = lcm(...steps);
+    console.log('Part2:', totalSteps, 'steps');
 })();
